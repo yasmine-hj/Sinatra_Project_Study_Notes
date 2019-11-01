2ec4a1bccd2017ebc03cb47ebc07232c9e9dafb4
yasmine-hj <yasminehajaji95@gmail.com>
2019-11-01 14:44:14 +0000

    adding home and new buttons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "reallydontdothis"
+    set :session_secret, "studynotessinatra"
   end
 
   get "/" do
